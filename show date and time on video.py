f1cf8e35df96b224eda9ae896bbc612cae9c6559
Anas Yaseen <89707493+AnasYas@users.noreply.github.com>
2021-08-31 13:36:52 +0300

    Update show date and time on video.py

--- show date and time on video.py
+++ show date and time on video.py
@@ -1,16 +1,21 @@
-import numpy as np
-events=(i for i in dir(cv2) if 'EVENT' in i)
-print(events)
-def click_event(event,x,y,flags,param):
-    if event==cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(img,(x,y),15,(255,0,0),-1)
-        points.append((x,y))
-        if len(points)>=2:
-            cv2.line(img,points[-1],points[-2],(0,255,255))
-        cv2.imshow('image',img)
-img=np.zeros((512,512,3),np.uint8)
-cv2.imshow('image',img)
-points=[]
-cv2.setMouseCallback('image',click_event)
+import cv2
+import datetime
+cap=cv2.VideoCapture(0,cv2.CAP_DSHOW)
+print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+while (cap.isOpened()):
+    ret,frame=cap.read()
+    if ret ==True:
+        font=cv2.FONT_HERSHEY_SIMPLEX
+        text='Width:'+str(cap.get(3)),'Height:'+str(cap.get(4))
+        datet=str(datetime.datetime.now())
+        frame=cv2.putText(frame, text, (10, 50) ,font, 1, (0, 255, 255), 2)
+        frame=cv2.putText(frame,datet,(10,100),font,1,(0,255,255),2)
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1)==ord(q):
+            break
+    else:
+        break
+cap.release()
 cv2.waitKey(0)
 cv2.destroyAllWindows()
